04ba3b187d6f2430c268a0369a531824ecf238ca
maousama1432 <131804292+maousama1432@users.noreply.github.com>
2023-11-24 14:01:07 -0400

    criando uma rota

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import Home from './Home'
+import Registro from './Registro'
+import Destaque from './Destaque'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
