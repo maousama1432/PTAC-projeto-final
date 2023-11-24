8a5d496878de0915609719cc0f2277aaeac097b4
maousama1432 <131804292+maousama1432@users.noreply.github.com>
2023-11-24 14:10:36 -0400

    colocando um iframe

---
+++ src/Destaque.jsx
@@ -0,0 +1,8 @@
+<iframe
+                    width="560"
+                    height="315"
+                    src={'https://www.youtube.com/embed/' + video.endereco.slice(17)}
+                    title={video.titulo}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                ></iframe>
