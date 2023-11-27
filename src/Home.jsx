09363f8274ed2f8fda551076c1514d089c21aa45
maousama1432 <131804292+maousama1432@users.noreply.github.com>
2023-11-27 14:21:46 -0400

    terminando de fazer os ultimos ajustes no home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,6 +8,17 @@ const Home = () => {
     setVideos(videosArray);
   }, []);
 
+  return (
+    <div>
+      <h2>Home</h2>
+      {videos.map((video) => (
+        <div key={video.id}>
+          <h3>{video.titulo}</h3>
+          <p>{video.descricao}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
