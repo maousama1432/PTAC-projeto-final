430c6e8e8d2cf237cbba9d6bb52d882159630838
maousama1432 <131804292+maousama1432@users.noreply.github.com>
2023-11-30 22:18:33 -0400

    Modificando o componente Main para incluir a rota dinâmica.

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Home from './Home'
-import Registro from './Registro'
-import Destaque from './Destaque'
+import Home from './Home';
+import Registro from './Registro';
+import Destaque from './Destaque';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <BrowserRouter>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/" element={<Registro />}></Route>
-        <Route path="/" element={<Destaque />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/cadastro" element={<Registro />} />
+        <Route path="/destaque" element={<Destaque />} />
+        <Route path="/:id" element={<Detalhes />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
