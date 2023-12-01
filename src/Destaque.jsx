77e60675a9e86e7feefaccd3677f94620544c16c
maousama1432 <131804292+maousama1432@users.noreply.github.com>
2023-11-30 22:16:40 -0400

    Alteração do componente Detalhes para renderizar os detalhes do vídeo

--- src/Destaque.jsx
+++ src/Destaque.jsx
@@ -1,8 +1,21 @@
-<iframe
-                    width="560"
-                    height="315"
-                    src={'https://www.youtube.com/embed/' + video.endereco.slice(17)}
-                    title={video.titulo}
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                ></iframe>
+import React from 'react';
+
+const Detalhes = ({ video }) => {
+  return (
+    <div>
+      <h2>Detalhes do Vídeo</h2>
+      <h3>{video.titulo}</h3>
+      <p>{video.descricao}</p>
+      <iframe
+        width="560"
+        height="315"
+        src={`https://www.youtube.com/embed/${video.url.slice(17)}`}
+        title={video.titulo}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default Detalhes;
