9310ed86b0c63cbd75100bf0671982f00bf3ff02
maousama1432 <131804292+maousama1432@users.noreply.github.com>
2023-11-30 22:34:56 -0400

    Criar o componente de registro de vídeo

--- src/Registro.jsx
+++ src/Registro.jsx
@@ -1,3 +1,42 @@
-useEffect(() => {
-    localStorage.setItem("musica", JSON.stringify(musica));
- }, [musica]);
+import React from 'react';
+
+const Registro = () => {
+  const [titulo, setTitulo] = useState('');
+  const [descricao, setDescricao] = useState('');
+  const [url, setUrl] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const novoVideo = {
+      titulo,
+      descricao,
+      url,
+    };
+
+    const videosArray = JSON.parse(localStorage.getItem('videos')) || [];
+    videosArray.push(novoVideo);
+    localStorage.setItem('videos', JSON.stringify(videosArray));
+
+    setTitulo('');
+    setDescricao('');
+    setUrl('');
+  };
+
+  return (
+    <div>
+      <h2>Registro de Vídeo</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Título:</label>
+        <input type="text" value={titulo} onChange={(e) => setTitulo(e.target.value)} />
+        <label>Descrição:</label>
+        <textarea value={descricao} onChange={(e) => setDescricao(e.target.value)} />
+        <label>URL:</label>
+        <input type="url" value={url} onChange={(e) => setUrl(e.target.value)} />
+        <button type="submit">Registrar</button>
+      </form>
+    </div>
+  );
+};
+
+export default Registro;
