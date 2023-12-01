b5307d2db545e2909c47397e87ab2d50d238eea3
maousama1432 <131804292+maousama1432@users.noreply.github.com>
2023-11-30 21:50:14 -0400

    Crie um novo arquivo chamado src/components/Card.jsx.

---
+++ src/components/Card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Card = ({ titulo, descricao, url }) => {
+  return (
+    <div className="card">
+      <h3>{titulo}</h3>
+      <p>{descricao}</p>
+      <iframe
+        width="560"
+        height="315"
+        src={"https://www.youtube.com/embed/" + url.slice(17)}
+        title={titulo}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default Card;
