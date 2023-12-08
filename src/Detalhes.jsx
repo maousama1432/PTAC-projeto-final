8e62a95661ba71f367f6c3690260ff14bf88a94f
maousama1432 <131804292+maousama1432@users.noreply.github.com>
2023-12-08 18:08:39 -0400

    navbar
    deixando mais "bonito"

--- src/Detalhes.jsx
+++ src/Detalhes.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 
 const Detalhes = ({ video }) => {
+  const tipoMedia = video.tipo || 'Vídeo';
+
   return (
     <div>
-      <h2>Detalhes do Vídeo</h2>
+      <h2>Detalhes do {tipoMedia}</h2>
       <h3>{video.titulo}</h3>
       <p>{video.descricao}</p>
-      <iframe
-        width="560"
-        height="315"
-        src={`https://www.youtube.com/embed/${video.url.slice(17)}`}
-        title={video.titulo}
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+      {tipoMedia === 'Música' ? (
+        <p>Letra da Música...</p>
+      ) : (
+        <iframe
+          width="560"
+          height="315"
+          src={`https://www.youtube.com/embed/${video.url.slice(17)}`}
+          title={video.titulo}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      )}
     </div>
   );
 };
