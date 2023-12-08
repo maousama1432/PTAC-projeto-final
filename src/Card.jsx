f52881d50761ea8d8a707f6f4df6988c4199f908
maousama1432 <131804292+maousama1432@users.noreply.github.com>
2023-12-08 16:13:15 -0400

    atualização
    tirando codigos que não precisa

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,16 +1,15 @@
-import React from "react";
+import React from 'react';
 
-const Card = ({ id, tituloVideo, descricao, url, imagem }) => {
+const Card = ({ titulo, descricao, url }) => {
   return (
     <div className="card">
-      <img src={imagem} alt="Imagem do vídeo" />
-      <h3>{tituloVideo}</h3>
+      <h3>{titulo}</h3>
       <p>{descricao}</p>
       <iframe
         width="560"
         height="315"
-        src={"https://www.youtube.com/embed/" + url.slice(17)}
-        title={tituloVideo}
+        src={`https://www.youtube.com/embed/${url.slice(17)}`}
+        title={titulo}
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
       ></iframe>
